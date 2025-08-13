a2cf05a3ec0258dbdbf89c5822d7c1583e09131e
antoniomarcodaddetta <a.daddetta@elis.org>
2025-08-13 17:40:36 +0200

    [BE] Add deploy on github

--- front-end/lenax-fe/src/main.jsx
+++ front-end/lenax-fe/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <BrowserRouter basename={import.meta.env.PUBLIC_URL || '/'}>
+        <App />
+    </BrowserRouter>
+);
--- front-end/lenax-fe/vite.config.js
+++ front-end/lenax-fe/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 
-// https://vite.dev/config/
+const rootPath = path.resolve();
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/<lenax-site>/', // Replace <repo-name> with your GitHub repo name
+  buildEnd() {
+    const src = path.join(rootPath, 'dist', 'index.html');
+    const dest = path.join(rootPath, 'dist', '404.html');
+    try {
+      fs.copyFileSync(src, dest);
+      console.log('404.html created successfully');
+    } catch (err) {
+      console.error('Failed to create 404.html', err);
+    }
+  },
+});
