0aea796f606956b8a8cd0fb22b902088e3db9991
antoniomarcodaddetta <a.daddetta@elis.org>
2025-08-19 15:40:10 +0200

    [FE] Fix deploy on github

--- front-end/lenax-fe/src/main.jsx
+++ front-end/lenax-fe/src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import App from './App.jsx'
 
 createRoot(document.getElementById("root")).render(
-    <BrowserRouter basename="/lenax-site/">
+    <HashRouter basename="/lenax-site/">
         <App />
-    </BrowserRouter>
+    </HashRouter>
 );
