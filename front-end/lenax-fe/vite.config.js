e94980e1238ff5dac592d938b5d029df632c1e10
antoniomarcodaddetta <a.daddetta@elis.org>
2025-08-19 11:51:53 +0200

    [FE] Fix deploy on github

--- front-end/lenax-fe/vite.config.js
+++ front-end/lenax-fe/vite.config.js
@@ -7,16 +7,21 @@ const rootPath = path.resolve();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/lenax-site/', // Replace <repo-name> with your GitHub repo name
-  buildEnd() {
-    const src = path.join(rootPath, 'dist', 'index.html');
-    const dest = path.join(rootPath, 'dist', '404.html');
-    try {
-      fs.copyFileSync(src, dest);
-      console.log('404.html created successfully');
-    } catch (err) {
-      console.error('Failed to create 404.html', err);
+  plugins: [
+    react(),
+    {
+      name: 'copy-index-to-404',
+      closeBundle: () => {
+        const src = path.join(rootPath, 'dist', 'index.html');
+        const dest = path.join(rootPath, 'dist', '404.html');
+        try {
+          fs.copyFileSync(src, dest);
+          console.log('404.html created successfully');
+        } catch (err) {
+          console.error('Failed to create 404.html', err);
+        }
+      }
     }
-  },
+  ],
+  base: '/lenax-site/',
 });
